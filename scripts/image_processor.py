15e024e1d8d58091f7dffc45728928ffd67d6894
Filip Hesse <Filip_Hesse@yahoo.de>
2020-11-27 12:03:41 +0100

    publish visibility

--- scripts/image_processor.py
+++ scripts/image_processor.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Int64
-from std_srvs.srv import SetBool
 
 import cv2
 import imutils
@@ -10,11 +8,14 @@ import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 
+from std_msgs.msg import Bool
+
 
 class NumberCounter:
     def __init__(self):
         self.counter = 0
-        self.pub = rospy.Publisher("camera1/image_processed", Image, queue_size=1)
+        self.pub_img = rospy.Publisher("camera1/image_processed", Image, queue_size=1)
+        self.pub_vis = rospy.Publisher("camera1/ball_visible", Bool, queue_size=1)
         self.number_subscriber = rospy.Subscriber("camera1/image_raw", Image, self.callback_raw_image)
 
         self.bridge = CvBridge()
@@ -35,6 +36,7 @@ class NumberCounter:
                                 cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
         center = None
+        visible = False
 
                 # only proceed if at least one contour was found
         if len(cnts) > 0:
@@ -50,11 +52,13 @@ class NumberCounter:
             if radius > 10:
                 # draw the circle and centroid on the frame,
                 # then update the list of tracked points
+                visible = True
                 cv2.circle(self.image, (int(x), int(y)), int(radius),
                            (0, 255, 255), 2)
                 cv2.circle(self.image, center, 5, (0, 0, 255), -1)
 
-        self.pub.publish((self.bridge.cv2_to_imgmsg(self.image, 'bgr8')))
+        self.pub_img.publish((self.bridge.cv2_to_imgmsg(self.image, 'bgr8')))
+        self.pub_vis.publish(visible)
 
 
 if __name__ == '__main__':
